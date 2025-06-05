1a88d894e4a99acc4d3f4cbaf6059652a4a32522
Vijaypratapsingh <vijaypratap1747singh@gmail.com>
2025-06-05 17:32:19 +0530

    fix login

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -53,11 +53,26 @@ function Navbar() {
         </div>
         <div className={open ? "menu active" : "menu"}>
           <a href="/">Home</a>
-          <a href="/">About</a>
-          <a href="/">Contact</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
           <a href="/">Agents</a>
-          <a href="/">Sign in</a>
-          <a href="/">Sign up</a>
+          {currentUser ? (
+            <div className="user">
+              <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
+              <span>{currentUser.username}</span>
+              <Link to="/profile" className="profile">
+                {number > 0 && <div className="notification">{number}</div>}
+                <span>Profile</span>
+              </Link>
+            </div>
+          ) : (
+            <>
+              <a href="/login">Sign in</a>
+              <a href="/register" className="register">
+                Sign up
+              </a>
+            </>
+          )}
         </div>
       </div>
     </nav>
